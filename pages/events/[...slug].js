23845ee044a2230b8e35bcc7ca91c0bf2bc767d4
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-23 15:50:54 +0500

    File-Based Routing Done

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilteredEvents = () => {
+  return (
+    <div>Filtered Events</div>
+  )
+}
+
+export default FilteredEvents
---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+
+function EventWithId() {
+
+    return (
+        <div>
+            <h1>Event With Id: </h1>
+        </div>
+    )
+}
+
+export default EventWithId
