3ddfc9d1d575cfcc3f27adbdd2f213d61e78e356
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-24 22:12:09 +0500

    Event Detail Page Done

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,27 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "./../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
-function EventWithId() {
+function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
 
-    return (
-        <div>
-            <h1>Event With Id: </h1>
-        </div>
-    )
+  if (!event) {
+    return <h1>No Event Found!</h1>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 
-export default EventWithId
+export default EventDetailPage;
