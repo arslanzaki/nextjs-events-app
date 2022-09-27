1937e6d0d043b741e491e7c8340ac30175df2d38
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-27 22:24:57 +0500

    Events Page Added With Filter Menu

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { getAllEvents } from "../../dummy-data";
 const EventsPage = () => {
+  const events = getAllEvents();
   return (
-    <div>Events Page</div>
-  )
-}
+    <>
+      <EventsSearch />
+      <EventList items={events} />
+    </>
+  );
+};
 
-export default EventsPage
+export default EventsPage;
