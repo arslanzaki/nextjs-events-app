d115b688a06039dfe576a7efe9a3027df551d1ce
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-29 22:47:21 +0500

    filter slug navigation added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,19 @@
+import { useRouter } from "next/router";
 import React from "react";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 const EventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
